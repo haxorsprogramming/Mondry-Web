60ba6336957e35efb132457073f0526f04f468de
Haxorsprogramming <haxors@uinsu.ac.id>
2021-12-27 12:47:17 +0700

    'update'

--- app/Http/Controllers/C_Home.php
+++ app/Http/Controllers/C_Home.php
@@ -8,6 +8,6 @@ class C_Home extends Controller
 {
     public function homePage()
     {
-        echo "Homepage";
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\C_Home;
+use App\Http\Controllers\C_Auth;
 
 Route::get('/', [C_Home::class, 'homePage']);
+Route::get('/login', [C_Auth::class, 'loginPage']);
